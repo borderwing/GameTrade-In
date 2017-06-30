0d785d030d920d9f88bc01638a63e54f56b73b0a
Yang Liu <lykavin@hotmail.com>
2017-06-30 09:03:06 +0800

    before merging with Ye

--- Code/Server/src/main/java/controller/UserController.java
+++ Code/Server/src/main/java/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
