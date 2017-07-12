7109c38caeddb35f790a90f36267d5049820ac6c
GhoultearZZW <zhangzhaowei96@gmail.com>
2017-07-12 15:55:30 +0800

    g2a python

--- Code/Server/src/main/java/script/python/g2a.py
+++ Code/Server/src/main/java/script/python/g2a.py
@@ -2,12 +2,15 @@ from bs4 import BeautifulSoup
 import urllib2
 from selenium import webdriver
 
-driver = webdriver.PhantomJS(executable_path='F:\python2\Scripts\phantomjs-2.1.1-windows\phantomjs.exe')
-driver.get("https://www.g2a.com/?search=dark+souls+3")
 
-soup = BeautifulSoup(driver.page_source, "html.parser")
-rightContent=soup.findAll('strong',attrs={'class':'mp-pi-price-min'})
-for price in rightContent:
-    for parent in price.parents:
-        if parent['class']=="mp-product-info":
-            print (price)
+def getEvaluatePoints(gameName):
+    gameName=gameName.replace("", "+")
+
+    driver = webdriver.PhantomJS(executable_path='F:\python2\Scripts\phantomjs-2.1.1-windows\phantomjs.exe')
+    driver.get("https://www.g2a.com/?search="+gameName)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    rightContent=soup.findAll('strong',attrs={'class':'mp-pi-price-min'})
+
+    priceDetail=rightContent[-1].get_text()
+    return (float(priceDetail[0:-2]))
